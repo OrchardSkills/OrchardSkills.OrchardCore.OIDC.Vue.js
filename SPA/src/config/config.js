f0b3b0f86be2ccfe02a68736d3f487d1204aea03
sales <sales@orchardskills.com>
2020-10-07 19:34:05 -0600

    cleanup up code

--- SPA/src/config/config.js
+++ SPA/src/config/config.js
@@ -1,8 +1,5 @@
 export const clientRoot = 'https://localhost:5002/'
 export const stsAuthority = 'https://localhost:44342/'
 export const clientId = 'code_flow_client_id'
-export const credentialsClientId = 'credentials_client_id'
-export const credentialsClientSecret = 'credentials_C1ient$ecret'
-export const credentialsClientGrantType = 'client_credentials'
 export const scope = 'openid profile api'
 export const responseType = 'code'
